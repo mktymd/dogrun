73e211c476c8ec16f224115bb6859fbc3b05a878
pub011 <pub011@pub011>
2026-09-03 14:28:08 +0900

    yokoyama14:30変更分

---
+++ dogrunproject/dogrunproject/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "dogruns/index.html")
+
+def mypage(request):
+    return render(request, "dogruns/mypage.html")
